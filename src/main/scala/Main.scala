4983a79993088bf52a886f0e1f32aeca10a775e1
Lens <lensgolda@gmail.com>
2025-10-21 01:17:43 +0200

    wip telegram

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,32 +2,36 @@ import java.io.IOException
 
 import _root_.config.Configuration.*
 import services.Cbr
+import services.messaging.TelegramLive
+import services.messaging.TelegramService
 import zio.*
 import zio.config.*
 import zio.config.magnolia.*
 import zio.config.typesafe.*
 import zio.http.*
+import zio.http.netty.NettyConfig
+import zio.http.netty.client.NettyClientDriver
+import zio.http.netty.server.NettyDriver
 import zio.logging.ConsoleLoggerConfig
 import zio.logging.LogFormat
 import zio.logging.backend.SLF4J
 import zio.logging.consoleJsonLogger
 import zio.logging.loggerName
-import services.messaging.TelegramLive
-import services.messaging.TelegramService
 
 object Gtbot4s extends ZIOAppDefault:
 
     override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] =
         Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
+    val clientConfig = ZClient.Config.default
+        .connectionTimeout(500.milliseconds)
+        .idleTimeout(1500.milliseconds)
+
     val app = for
         _ <- ZIO.logInfo(">>> APPLICATION START <<<") @@ loggerName("Gtbot4s")
         appConfig <- ZIO.service[AppConfig]
         _ <- ZIO.logInfo(s"AppConfig: ${appConfig}") @@ loggerName("Gtbot4s")
         // ratesList <- ZIO.serviceWithZIO[Cbr](_.fetchAll)
-        // _ <- ZIO.foreachDiscard(ratesList)(rate =>
-        //     ZIO.logInfo(s"Rate: ${rate}")
-        // )
         _ <- ZIO.serviceWithZIO[TelegramService](_.sendMessage(s"Testing"))
     // result <- ZIO.collectAllPar(List(effect1, effect2))
     yield ()
@@ -41,5 +45,10 @@ object Gtbot4s extends ZIOAppDefault:
           AppConfig.layer,
           //   Cbr.live,
           TelegramService.live,
-          Client.default
+          // Zio Client & settings
+          Client.customized,
+          DnsResolver.default,
+          NettyClientDriver.live,
+          ZLayer.succeed(NettyConfig.default),
+          ZLayer.succeed(clientConfig)
         )
